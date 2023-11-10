1e4090e97d5edf12292749801bb0d029f945d7ce
DvaliElgudja <dvali.Elgudja@gmail.com>
2023-11-10 03:31:04 +0100

    ajout modal et descriptions

--- script.js
+++ script.js
@@ -62,8 +62,25 @@ window.addEventListener('scroll', function() {
 });
 
 
+//------------Modal -----------//
 
+// JavaScript to handle modal opening and closing
+function openModal(modalId) {
+  var modal = document.getElementById(modalId);
+  modal.style.display = 'block';
+}
 
+function closeModal(modalId) {
+  var modal = document.getElementById(modalId);
+  modal.style.display = 'none';
+}
+
+// Close modal when clicking outside of it
+window.onclick = function(event) {
+  if (event.target.className === 'modal') {
+    event.target.style.display = 'none';
+  }
+}
 
 
 
